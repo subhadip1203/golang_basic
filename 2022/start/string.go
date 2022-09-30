ca73e32347084792c3bb0bea33eec7c1eb42b914
subhadip1203 <subahdip1203@gmail.com>
2022-09-30 15:02:44 -0700

    basic varuiable

---
+++ 2022/start/string.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var print = fmt.Println
+
+func main() {
+	var vName string = "subhadip"
+	var vName2 = "subhadip"
+	vName3 := "subahdip"
+
+	print(vName, vName2, vName3)
+}
