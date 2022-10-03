8811db0ca587733ecfe5d362ee8f8720c315e592
subhadip1203 <subhadip1203@gmail.com>
2022-10-02 23:46:40 -0700

    staring methods go lang

--- 2022/start/string.go
+++ 2022/start/string.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 var print = fmt.Println
@@ -12,4 +13,18 @@ func main() {
 	vName3 := "subahdip"
 
 	print(vName, vName2, vName3)
+
+	sV1 := "a monster"
+	sV2 := strings.NewReplacer("a", "another").Replace(sV1)
+	print(sV2)
+	print("Length of string :", len(sV2))
+
+	print("contains another:", strings.Contains(sV2, "another"))
+
+	print("Index of o :", strings.Index(sV2, "o"))
+
+	print("Replace char o :", strings.Replace(sV2, "o", "O", 1))  // single time reolace , so 1
+	print("Replace char o :", strings.Replace(sV2, "o", "O", 2))  // single time reolace , so 2
+	print("Replace char s :", strings.Replace(sV2, "e", "E", -1)) // all  reolace , so -1
+
 }
