8b333215cc21808883886758d77f621b612f9100
subhadip1203 <subhadip1203@gmail.com>
2022-12-26 21:31:08 -0800

    go struct and functions

--- 2022/start/9_struct.go
+++ 2022/start/9_struct.go
@@ -11,15 +11,35 @@ func main() {
 		firstName string
 		lastName  string
 		age       int
-		subjects  []string
+		address   []string
 	}
 
 	p1 := Person{
 		firstName: "subhadip",
 		lastName:  "pahari",
 		age:       30,
-		subjects:  []string{"math", "physics", "cs"},
+		address:   []string{"math", "physics", "cs"},
 	}
 
 	print(p1)
+
+	// promotion in struct
+	type Student struct {
+		person Person
+		rollNo int
+	}
+
+	s1 := Student{
+		person: Person{
+			firstName: "subhadip",
+			lastName:  "pahari",
+			age:       30,
+			address:   []string{"fullerton", "california"},
+		},
+		rollNo: 17,
+	}
+
+	print(s1)
+	print(s1.person.firstName)
+
 }
