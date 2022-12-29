e97a421919aeccba5dcc86154112f24e9dd32eee
subhadip1203 <subhadip1203@gmail.com>
2022-12-28 22:45:15 -0800

    function in go

--- 2022/start/10_functions.go
+++ 2022/start/10_functions.go
@@ -7,5 +7,19 @@ import (
 var print = fmt.Println
 
 func main() {
+	defer runLast()
+	print(oneResult())
+	print(multiResult())
+}
 
+func oneResult() string {
+	return "ok"
+}
+
+func multiResult() (string, string) {
+	return "ok", "another"
+}
+
+func runLast() {
+	print("run at last for => Defer")
 }
