7a9793b1b3ce2489eaf3a094e4eb3285aae17e4c
subhadip1203 <subhadip1203@gmail.com>
2023-01-05 19:19:47 -0800

    go routines

--- 2022/start/13_goroutines.go
+++ 2022/start/13_goroutines.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
 var print = fmt.Println
 
-func printHi() {
-	print("Hi 1")
+func printHi(s string) {
+	for i := 0; i < 3; i++ {
+		print(s)
+		time.Sleep(time.Millisecond * 100)
+	}
+	wg.Done()
 }
 
 func main() {
 	// the key word go used for go routines
-	go printHi()
+	wg.Add(1)
+	go printHi("hi")
+	wg.Add(1)
+	go printHi("hello")
+	wg.Wait()
+	wg.Add(1)
+	go printHi("subhadip")
+	wg.Wait()
 
-	print("Hi 2")
-	time.Sleep(time.Second)
 }
