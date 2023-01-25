7ca75a621ced36b62b4a45916ecf68910fa39f86
subhadip1203 <subhadip1203@gmail.com>
2023-01-24 21:46:57 -0800

    rest API start

--- 2023/webDev/server2/main.go
+++ 2023/webDev/server2/main.go
@@ -1,35 +1,116 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type Movie struct {
+	ID       string    ` json: "id" `
+	ISBN     string    ` json: "isbn" `
+	Title    string    `json: "title" `
+	Director *Director `json: "director" `
+}
+
+type Director struct {
+	FirstName string ` json: "firstname" `
+	LastName  string ` json: "lastname" `
+}
+
+var movies []Movie
+
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/about", aboutHandler)
-	http.HandleFunc("/form", aboutForm)
+
+	// creating Fake movies
+	movies = append(movies, Movie{ID: "1", ISBN: "abcd1", Title: "Title 1", Director: &Director{FirstName: "Director 1 first", LastName: "Director 1 last"}})
+	movies = append(movies, Movie{ID: "2", ISBN: "abcd2", Title: "Title 2", Director: &Director{FirstName: "Director 2 first", LastName: "Director 2 last"}})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", ping).Methods("GET")
+	r.HandleFunc("/movies", getAllMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovies).Methods("POST")
+	r.HandleFunc("/movies", updateMovies).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
 
 	fmt.Println("starting server at http://localhost:8000")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	if err := http.ListenAndServe(":8000", r); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "about us page")
+// function for route : "/" GET
+func ping(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "{status:1}")
 }
 
-func aboutForm(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "parseForm error : %v", err)
-		return
+// get all the movies details
+func getAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+// get a perticular movie
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			break
+		}
 	}
-	fmt.Fprintf(w, "form parse successfully \n")
-	name := r.FormValue("name")
-	address := r.FormValue(("address"))
-	fmt.Fprintf(w, "name= %s \n", name)
-	fmt.Fprintf(w, "address= %s \n", address)
+}
+
+// create Movie
+func createMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	var newMovie Movie
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&newMovie)
+	if err != nil {
+		panic(err)
+	}
+	movies = append(movies, newMovie)
+	json.NewEncoder(w).Encode(newMovie)
+}
+
+// update Movie
+func updateMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	var existingMovie Movie
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&existingMovie)
+	if err != nil {
+		panic(err)
+	}
+
+	var existingMovieIndex int = -1
+	for index, item := range movies {
+		if item.ID == existingMovie.ID {
+			existingMovieIndex = index
+			break
+		}
+	}
+
+	movies[existingMovieIndex] = existingMovie
+	json.NewEncoder(w).Encode(existingMovie)
+
+}
+
+// Delete a movie
+func deleteMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
 }
