c120f07370666940bb383068f5abf366609bc947
subhadip1203 <subhadip1203@gmail.com>
2023-02-09 21:37:12 -0800

    get all book controller

--- 2024/webDev/project1/pkg/controllers/book_controller.go
+++ 2024/webDev/project1/pkg/controllers/book_controller.go
@@ -1,12 +1,26 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	model "github.com/subhadip1203/golang_basic/2024/webDev/project1/pkg/models"
 )
 
 func GetAllBooks(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, "{status:1 , message: 'get all books'}")
+	res.Header().Set("content-type", "appilaction/json")
+
+	books := model.GetAllBooks()
+	jsonBooks, err := json.Marshal(books)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(res, "{status:0, error: 1}")
+	} else {
+		res.WriteHeader(http.StatusOK)
+		res.Write(jsonBooks)
+	}
+
 }
 
 func GetBook(res http.ResponseWriter, req *http.Request) {
